f0ef7c9b6f1d6c7c9d798132988b515aee468c19
ahmdaboelhassan <ahmedaboelhassan1000@gmail.com>
2024-09-20 21:14:45 +0300

    Modified Group JQuery DataTable With Summary

--- JS - JQuery/Group JQuery DataTable With Summary.js
+++ JS - JQuery/Group JQuery DataTable With Summary.js
@@ -13,6 +13,7 @@ function GroupDataTableByColumnsIndexArray(
   if ($.fn.DataTable.isDataTable(tableSelector)) {
     $(tableSelector).DataTable().destroy();
   }
+
   if (groupColumnIndex != "") {
     var table = $(tableSelector).DataTable({
       responsive: true,
@@ -23,55 +24,47 @@ function GroupDataTableByColumnsIndexArray(
         var api = this.api();
         var rows = api.rows().nodes();
         var groupSums = Array(ColumnsArray.length).fill(0);
-        var totalColumnsCount = api.columns(":visible").count();
+        var totalColumnsCount = api.columns().count();
+
         api
           .column(groupColumnIndex)
           .data()
           .each(function (group, i) {
             if (last !== group) {
               if (last !== null && ColumnsArray.length > 0) {
-                var sumRow =
-                  '<tr class="sum-row" style="background-color:#f0f8ff;font-weight:bold;color:#337ab7;height:20px">';
-                sumRow += '<td colspan="' + ColumnsArray[0] + '"></td>';
-                for (var j = 0; j < groupSums.length; j++) {
-                  // check if corresponing th is hidden
-                  var thElement = $("th").eq(ColumnsArray[j]);
-                  var displayStyle =
-                    thElement.css("display") === "none" ? "display:none;" : "";
-                  sumRow +=
-                    '<td style="' +
-                    displayStyle +
-                    '">' +
-                    groupSums[j].toFixed(2) +
-                    "</td>";
-                }
-                var remianCells =
-                  totalColumnsCount - ColumnsArray[ColumnsArray.length - 1] - 1;
-                if (remianCells > 0) {
-                  sumRow += '<td colspan="' + remianCells + '"></td>';
-                }
-                sumRow += "</tr>";
+                var sumRow = GetSumRow(
+                  tableSelector,
+                  ColumnsArray,
+                  groupSums,
+                  totalColumnsCount
+                );
                 $(rows).eq(i).before(sumRow);
               }
+
               // Add the group header row
-              $(rows)
-                .eq(i)
-                .before(
-                  '<tr class="group"><td colspan="' +
-                    totalColumnsCount +
-                    '" style="padding:10px;background-color:#EEE;">' +
-                    group +
-                    "</td></tr>"
-                );
+              var groupRow = GetGroupRow(group, totalColumnsCount);
+              $(rows).eq(i).before(groupRow);
+
               last = group;
               groupSums = Array(ColumnsArray.length).fill(0); // Reset the sums for the new group
             }
 
             // Add the current row's values to the group sums
             if (ColumnsArray.length > 0) {
-              var rowData = api.row(api.row($(rows).eq(i)).index()).data();
+              var rowData = api.row($(rows).eq(i)).data();
               for (var j = 0; j < ColumnsArray.length; j++) {
                 groupSums[j] += parseFloat(rowData[ColumnsArray[j]]) || 0;
+              }
+
+              // If this is the last row, add the sum row for the last group
+              if (i === rows.length - 1) {
+                var sumRow = GetSumRow(
+                  tableSelector,
+                  ColumnsArray,
+                  groupSums,
+                  totalColumnsCount
+                );
+                $(rows).eq(i).after(sumRow);
               }
             }
           });
@@ -80,8 +73,44 @@ function GroupDataTableByColumnsIndexArray(
   } else {
     var table = $(tableSelector).DataTable({
       responsive: true,
-      displayLength: 25,
-      order: [[0, "desc"]],
+      displayLength: 10,
+      order: [[0, "asc"]],
     });
   }
+}
+
+function GetSumRow(tableSelector, ColumnsArray, groupSums, totalColumnsCount) {
+  var sumRow =
+    '<tr class="sum-row" style="background-color:#f0f8ff;font-weight:bold;color:#337ab7;height:20px">';
+
+  sumRow += '<td colspan="' + ColumnsArray[0] + '"></td>';
+
+  for (var j = 0; j < groupSums.length; j++) {
+    // check if corresponding TH Element is hidden
+    var thElement = $(tableSelector + " th").eq(ColumnsArray[j]);
+    var isDisplayed = thElement.css("display") !== "none";
+    if (isDisplayed) {
+      sumRow += "<td>" + groupSums[j].toFixed(2) + "</td>";
+    }
+  }
+
+  var remianCells =
+    totalColumnsCount - ColumnsArray[ColumnsArray.length - 1] - 1;
+
+  if (remianCells > 0) {
+    sumRow += '<td colspan="' + remianCells + '"></td>';
+  }
+
+  sumRow += "</tr>";
+  return sumRow;
+}
+
+function GetGroupRow(groupName, totalColumnsCount) {
+  return (
+    '<tr class="group"><td colspan="' +
+    totalColumnsCount +
+    '" style="padding:10px;background-color:#EEE;">' +
+    groupName +
+    "</td></tr>"
+  );
 }
