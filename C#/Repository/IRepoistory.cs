de380ea5de2d194674e01c95434beb5fbc7bb6fd
ABO <89804749+Ahmdaboelhassan@users.noreply.github.com>
2024-12-10 23:54:29 +0200

    Add files via upload

---
+++ C#/Repository/IRepoistory.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Application.IRepository;
+public interface IRepository<T>
+{
+    // Queries
+    Task<T> Get(Expression<Func<T, bool>> criteria, params string[] includes);
+    Task<List<T>> GetAll(params string[]? includes);
+    Task<List<T>> GetAll(Expression<Func<T, bool>> criteria, params string[]? includes);
+    Task<IEnumerable<O>> SelectAll<O>(Expression<Func<T, bool>> criteria, Expression<Func<T, O>> columns, params string[]? includes);
+    Task<bool> Exists(Expression<Func<T, bool>> criteria);
+
+    // Command
+    Task AddAsync(T element);
+    Task DeleteAsync(T element);
+    Task UpdateAsync(T element);
+}
---
+++ C#/Repository/Repository.cs
@@ -0,0 +1,81 @@
+﻿using Application.IRepository;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repository;
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly DbSet<T> _set;
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context) 
+    {
+        _context = context;
+        _set = _context.Set<T>();
+    }
+
+    public Task<T> Get(Expression<Func<T, bool>> criteria, params string[] includes)
+    {
+        var query = _set.AsNoTracking().Where(criteria);
+        foreach (var item in includes)
+            query = query.Include(item);
+
+        return query.FirstOrDefaultAsync();
+    }
+    public Task<List<T>> GetAll(Expression<Func<T, bool>> criteria, params string[]? includes)
+    {
+        var query = _set
+            .Where(criteria)
+            .AsNoTracking();
+
+        foreach (var item in includes)
+            query = query.Include(item);
+
+        return query.ToListAsync();
+    }
+    public Task<List<T>> GetAll(params string[]? includes)
+    {
+        var query = _set.AsNoTracking();
+
+        foreach (var item in includes)
+            query = query.Include(item);
+
+        return query.ToListAsync();
+    }
+    public async Task<bool> Exists(Expression<Func<T, bool>> criteria)
+    {
+        return await _set.AnyAsync(criteria);
+    }
+    public Task AddAsync(T element)
+    {
+        _set.AddAsync(element);
+        return Task.CompletedTask;
+    }
+    public Task UpdateAsync(T element)
+    {
+        _set.Update(element);
+        return Task.CompletedTask;
+    }
+    public Task DeleteAsync(T element)
+    {
+        _set.Remove(element);
+        return Task.CompletedTask;
+    }
+    public async Task<IEnumerable<O>> SelectAll<O>(Expression<Func<T, bool>> criteria, Expression<Func<T, O>> columns, params string[]? includes)
+    {
+        var query = _set
+            .Where(criteria)
+            .AsNoTracking();
+
+        foreach (var item in includes)
+        {
+            query = query.Include(item);
+        }
+
+        return await query
+            .Select(columns)
+            .ToListAsync();
+    }
+}
